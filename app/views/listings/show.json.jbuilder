d88f1a2d115a373662bb3c82a6324df01af82e7e
Prelang Builder <builder@prelang.com>
2016-01-21 08:48:45 +0000

    Generated scaffolding for model 'Listing' via Rails

---
+++ app/views/listings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @listing, :id, :title, :rent_property_id, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :listings
+
   resources :auctions
 
   resources :rent_properties
